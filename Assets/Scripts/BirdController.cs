aaba58b3f964bd79b12f5fc156f7b49521b14eb3
Aimini05 <cureblossommail@gmail.com>
2025-04-04 16:42:52 +0200

    Instead of Clamp use manual position correction
    Clamp overrides the physics. Impulse made the bird rocket to the space

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -9,12 +9,20 @@ public class BirdController : MonoBehaviour
     private Rigidbody2D rigidBody;
     private bool isFlying = false;
 
-    private 
+    private float minHeight;
+    private float maxHeight;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+
+        Camera camera = Camera.main;
+        float camHeight = camera.orthographicSize;
+
+        minHeight = camera.transform.position.y - camHeight;
+        maxHeight = camera.transform.position.y + camHeight;
+
     }
 
     private void Update()
@@ -36,6 +44,19 @@ public class BirdController : MonoBehaviour
             //rigidBody.velocity = Vector2.up * velocity;
 
         }
+
+        if (transform.position.y < minHeight)
+        {
+            transform.position = new Vector3(transform.position.x, minHeight, transform.position.z);
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0f); // stop downward velocity
+        }
+
+        if (transform.position.y > maxHeight)
+        {
+            transform.position = new Vector3(transform.position.x, maxHeight, transform.position.z);
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0f); // stop upward velocity
+        }
+
 
     }
 }
