9354803406ad5da934ee9ef1b67a57018e9c893b
Aimini05 <cureblossommail@gmail.com>
2025-04-04 16:35:53 +0200

    Change Camera to Orthographic

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -9,6 +9,8 @@ public class BirdController : MonoBehaviour
     private Rigidbody2D rigidBody;
     private bool isFlying = false;
 
+    private 
+
     // Start is called before the first frame update
     void Start()
     {
