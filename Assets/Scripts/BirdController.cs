e988c53d4d9b2f01aed40987091e7c3814267dbc
Aimini05 <cureblossommail@gmail.com>
2025-04-04 15:49:32 +0200

    Edit BirdController.cs

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class BirdController : MonoBehaviour
 {
+
+    public float velocity = 1f;
+    public Rigidbody2D rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+        if (Input.GetButton(0))
+        {
+            rigidBody.velocity = Vector2.up * velocity;
+        }
+        */
     }
 }
