f7d4f1808bd740d3d47100cebb91c35586494022
Fulip <98706116+DaFxqx19@users.noreply.github.com>
2025-05-09 23:41:27 +0200

    Speed part 3
    The background now moves correctly
    
    - Credit to "DaF_Dango", helped come up with the concept

--- Assets/Scripts/BackgroundDetector.cs
+++ Assets/Scripts/BackgroundDetector.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BackgroundDetector : MonoBehaviour
@@ -10,6 +11,23 @@ public class BackgroundDetector : MonoBehaviour
     {
         if (other.CompareTag("Background"))
         {
+            int amount = 0;
+            float highestX = 0;
+
+            GameObject[] gameObjects = GameObject.FindObjectsOfType<GameObject>();
+            Object[] lista = FindObjectsOfType(typeof(GameObject));
+            foreach (GameObject obj in gameObjects)
+            {
+                if (obj.CompareTag("Background"))
+                {
+                    amount += 1;
+                    if (highestX < obj.transform.position.x)
+                    {
+                        highestX = obj.transform.position.x;
+                    }
+                }
+            }
+            location = new Vector3(highestX + 3, 0, 0);
             //Debug.Log("Entered");
             other.transform.position = location;
         }
