2fb5125b7ed0c43cb538e84d74e02bf89b5dadad
MicrowaveOfficial <ludvigunderlaten@gmail.com>
2025-05-15 11:39:56 +0200

    Partiklar
    När man träffar ett skott
    Lite mer skak när man blir träffad

--- Assets/Scripts/EnemyDestroy.cs
+++ Assets/Scripts/EnemyDestroy.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class EnemyDestroy : MonoBehaviour
 {
+    public GameObject bigBoom;
+    public GameObject boomParticle;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Projectile"))
         {
             Destroy(collision.gameObject);
             Destroy(gameObject);
+            Instantiate(boomParticle, collision.gameObject.transform.position, Quaternion.identity);
+            //Instantiate(bigBoom, collision.gameObject.transform.position, Quaternion.identity);
         }
     }
 }
